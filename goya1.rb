810de0e19f5bbb725f29ff57c76faaadda92de9e
Your Name <nadinantire@gmail.com>
2019-08-28 15:01:22 +0200

    web technology

---
+++ goya1.rb
@@ -0,0 +1,12 @@
+
+require 'cgi'
+cgi = CGI.new
+cgi.out("type" => "text/html", "charset" => "UTF-8") {
+  get = cgi['goya1']
+  "<html>
+    <body>
+      <p>The size of the bitter gourd and the information when quality is false:</p>
+      String：#{get}
+    </body>
+  </html>"
+}
---
+++ indicate.rb
@@ -0,0 +1,13 @@
+require 'cgi'
+cgi = CGI.new
+
+cgi.out("type" => "text/html", "charset" => "UTF-8") {
+   
+    get = cgi['input']
+    "<html>
+      <body>
+        <p>The received string is as follows</p>
+        <p>String：#{get}</p>
+      </body>
+    </html>"
+  }
